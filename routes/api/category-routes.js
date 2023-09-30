116ffa6da0c1e8ca6782fca7f5d1ef671f84384f
mhorton <horton-michael@users.noreply.github.com>
2023-09-29 21:57:41 -0400

    adds route to get a single Category by id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -19,6 +19,18 @@ router.get("/", async (req, res) => {
 router.get("/:id", async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const catData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    if (!catData) {
+      res.status(404).json({ message: "No category found with that id!" });
+      return;
+    }
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post("/", (req, res) => {
