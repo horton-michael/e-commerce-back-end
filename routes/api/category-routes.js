af4e9ecf45d52f905173891e0a2911984c55ca01
mhorton <horton-michael@users.noreply.github.com>
2023-09-29 22:04:20 -0400

    adds route to update a Category by id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -43,8 +43,20 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a category by its `id` value
+  try {
+    const catData = await Category.update(req.body, {
+      where: { id: req.params.id },
+    });
+    if (!catData) {
+      res.status(404).json({ message: "No category found with that id!" });
+      return;
+    }
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.delete("/:id", (req, res) => {
